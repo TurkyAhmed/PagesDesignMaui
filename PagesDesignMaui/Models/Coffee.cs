4db33f9898d648b9534a5c5988ca65613a6b4a42
TR17 <tasg1818@hotmail.com>
2025-07-07 17:06:11 +0300

    add coffee model class

---
+++ PagesDesignMaui/Models/Coffee.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace PagesDesignMaui.Models
+{
+    public class Coffee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string Price { get; set; }
+        public string Category { get; set; }
+        public string Rating { get; set; }
+
+    }
+}

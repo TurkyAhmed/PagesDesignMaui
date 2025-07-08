b7f52ceef312e27b2d07cdff0175c19662c7d6ec
TR17 <tasg1818@hotmail.com>
2025-07-08 10:32:59 +0300

    update coffee model

--- PagesDesignMaui/Models/Coffee.cs
+++ PagesDesignMaui/Models/Coffee.cs
@@ -8,9 +8,9 @@ namespace PagesDesignMaui.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
-        public string Price { get; set; }
+        public decimal Price { get; set; }
         public string Category { get; set; }
-        public string Rating { get; set; }
+        public decimal Rating { get; set; }
 
     }
 }
